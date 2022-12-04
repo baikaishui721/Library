42371caa9af838063b029dc9b6d5d0e82fdf605b
baikaishui721 <111034960+baikaishui721@users.noreply.github.com>
2022-12-04 19:03:07 +0800

    Add files via upload

---
+++ java/com/example/librarymanagementsystem/Book.java
@@ -0,0 +1,16 @@
+package com.example.librarymanagementsystem;
+
+public class Book {
+    public int coverresourceid;
+    public char title;
+
+    public Book(String s, int book_2) {
+    }
+
+    public void setTitle(String name) {
+    }
+
+    public char getTitle() {
+        return 0;
+    }
+}
---
+++ java/com/example/librarymanagementsystem/InputBookListActivity.java
@@ -0,0 +1,5 @@
+package com.example.librarymanagementsystem;
+
+public class InputBookListActivity {
+    public static final char RESULT_CODE =3;
+}
